3ea0aabd2e07bac6cfa662d7e172a7cd96655389
tomerKalif <tomer.kalif2003@gmail.com>
2022-01-19 14:55:27 +0200

    added Iserver

--- PingPong/ServerLogic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ServerLogic
-{
-    public class Class1
-    {
-    }
-}
---
+++ PingPong/ServerLogic/Iserver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerLogic
+{
+    interface Iserver
+    {
+        public void StartServer(string ip, string portNumber);
+
+        public Task ListenForClients();
+
+        public Task ReciveFromClient(Socket client);
+
+        public Task SendToClient(Socket client, byte toSend);
+    }
+}
